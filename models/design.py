caf368d5ea1510efbb243d2b683e933bb77d1a62
frarlo <farmentalo8@gmail.com>
2024-02-13 14:21:30 +0100

    minor tweaks

--- models/design.py
+++ models/design.py
@@ -2,12 +2,12 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class Design(models.Model):
-    """ Clase que implementa un Proyecto"""
+    """ Clase que implementa un Diseño"""
 
     _name = 'gestion_proyectos.design'
     #_inherit = 'gestion_proyectos.design' # one2one # Descomentar esta linea causa error
     _description = 'Define cada uno de los diseños relacionados con los proyectos existentes en la empresa'
-    _rec_name = 'project_id'
+    _rec_name = 'id'
 
     # Cada diseño tiene un project_id:
     project_id = fields.Many2one('gestion_proyectos.project')
@@ -19,37 +19,78 @@ class Design(models.Model):
          ('tipo3', 'Aerotermia'),
          ('tipo4', 'Climatización'),
          ('tipo5', 'Minieólica'),
-         ('tipo6', 'Supercargador automoción'),
-         ('tipo7', 'Instalación de baterías')
+         ('tipo6', 'Supercargador automoción')
+         #('tipo7', 'Instalación de baterías') not sure
           ]
     )
 
     # Campos comunes a todos los diseños:
-
     client_observations = fields.Text()
-
     engineer_observations = fields.Text()
 
     # Campos dependiendo del tipo de proyecto escogido en Selection:
-
     recommended_location = fields.Char()
-
     square_meters = fields.Float()
+    recommended_solars = fields.Float(compute='_get_solars', readonly=True)
+    recommended_batteries = fields.Float(compute='_get_batteries', readonly=True)
+    recommended_aerotermics = fields.Float(compute='_get_aerotermics', readonly=True)
+    recommended_conditioners = fields.Float(compute='_get_conditioners', readonly=True)
 
-    recommended_solars = fields.Float(compute='_get_solars', readonly=True) #_compute
 
-    quote_amount = fields.Float() # _compute
+    quote_amount = fields.Float(compute='_get_quote', readonly=True) # _compute
 
-    #TODO
+    # Calcula el número de placas solares (térmicas y fotovoltaicas) sobre los metros cuadrados:
+    @api.depends('square_meters')
     def _get_solars(self):
         for record in self:
             if record.square_meters:
                 record.recommended_solars = record.square_meters // 10
             else:
                 record.recommended_solars = 0
+    
+    # Calcula el número de aparatos de aerotermia según los metros cuadrados de instalación:
+    @api.depends('square_meters')
+    def _get_aerotermics(self):
+        for record in self:
+            if record.square_meters:
+                record.recommended_aerotermics = record.square_meters // 200
+            else:
+                record.recommended_aerotermics = 0
+    
+    # Calcula el número de baterias:
+    @api.depends('recommended_solars')            
+    def _get_batteries(self):
+        for record in self:
+            if record.recommended_solars:
+                record.recommended_batteries = record.recommended_solars // 5    # Example, not sure
+            else:
+                record.recommended_batteries = 0
+    
+    # Calcula el número de aires acondicionados:
+    @api.depends('square_meters')
+    def _get_conditioners(self):
+        for record in self:
+            if record.square_meters:
+                record.recommended_conditioners = record.square_meters // 100
+            else:
+                record.recommended_conditioners = 0
+
+    # TODO - Calcula el presupuesto de instalación (debería añadirse al precio de todo los productos y materiales obviamente):
+    @api.depends('recommended_aerotermics','recommended_solars','recommended_batteries')
+    def _get_quote(self):
+        for record in self:
+            if record.recommended_aerotermics:
+                record.quote_amount = record.recommended_aerotermics * 350
+            if record.recommended_solars:
+                record.quote_amount = record.recommended_solars * 200
+            if record.recommended_batteries:
+                record.quote_amount = record.recommended_batteries * 50
+            else:
+                record.quote_amount = 0
+
 
 
     # TODO - Restricción SQL:
-    _sql_constraints = [
-        ('design_uniq', 'unique(design)', 'No puede haber otro proyecto con este diseño')
-    ]
+    # _sql_constraints = [
+    #     ('design_uniq', 'unique(project_id)', 'No puede haber otro proyecto con este diseño'),
+    # ]
--- models/project.py
+++ models/project.py
@@ -7,11 +7,20 @@ class Project(models.Model):
 
     _name = 'gestion_proyectos.project'
     _description = 'Define cada uno de los proyectos existentes en la empresa'
-    _rec_name = 'id'
+    _rec_name = 'auto_id'
 
-    # # Código del proyecto alfanumérico # TODO
-    #id = fields.Char(compute='_generador_id')
+    # Código del proyecto alfanumérico
+    auto_id = fields.Char(compute='_generate_id')
 
+    @api.depends('cliente_id','ingeniero_id','fecha_inicio')
+    def _generate_id(self):
+        for record in self:
+            if record.cliente_id and record.ingeniero_id and record.fecha_inicio:
+                auto_id = f"MXPOW{record.cliente_id.name[2:]}{record.ingeniero_id.name}{record.fecha_inicio.strftime('%m%d')}"
+                record.auto_id = auto_id.strip().upper() # Improve
+            else:
+                record.auto_id = "Creando registro..."
+    
     # Cliente:
     cliente_id = fields.Many2one('res.partner', string = 'Cliente', required=True, domain="[('category_id', '=', 'Cliente')]")
 
@@ -22,27 +31,23 @@ class Project(models.Model):
     # Ingeniero:
     ingeniero_id = fields.Many2one('hr.employee', string= 'Ingeniero', required=True, domain="[('job_title', '=', 'Ingeniero')]")  
 
-    # Fase - Al crear se debería autoseleccionar Planificación y no poder cambiarse.                        #TODO que sea un stage.id
+    # Fase - Al crear se debería autoseleccionar Planificación y no poder cambiarse.                        #TODO que sea un stage.id?
     fase = fields.Selection([('plan', 'Planificación'), ('exec', 'Ejecución'), ('end', 'Finalización')],
                             default= 'plan',    # Fase predeterminada de inicio
-                            readonly= True)     # El usuario no puede cambiarla
+                            readonly= True)     # El usuario no puede cambiarla manualmente
 
-    # TODO subapartados del proyecto (pantallas 1-7)
-
-    # 1. Tareas - El proyecto tiene una lista de tareas que implementar (suelen ser globales y compartidas entre proyectos)
+    # Pagina 1. Tareas - El proyecto tiene una lista de tareas que implementar (TODO: Añadir automáticamente, actualizar sus valores
+    # conforme vayamos realizando tareas de planificación de forma automática)
 
     tareas_ids = fields.Many2many('gestion_proyectos.task', string="Tareas")
 
-    
-    # TODO Hacer un one2one manual AQUÍ - Cada proyecto tiene un diseño específico. Necesita un compute y un inverse para hacer get/set:
+    # Pagina 2. Diseño del Proyecto - Cada proyecto tiene un diseño específico. Necesita un compute y un inverse para hacer get del diseño y settearlo
+    # Debe hacerse con un one2one manual - ¿Debe crearse el proyecto sin diseño? TODO
 
-    # 2. Diseño - El proyecto tiene un diseño específico y único acorde al proyecto (únicas): (se tiene que poder crear sin diseño ya que guardará todo en la BD)
-    #design_id = fields.Many2one('gestion_proyectos.design') #TODO _compute e inverse
+    design_id = fields.Many2one('gestion_proyectos.design', compute='_get_project_design', inverse='_set_project_design', store=True)
     
-    # One2one manual:
-    design_id = fields.Many2one('gestion_proyectos.design', compute='_get_project_design', inverse='_set_project_design')
+    # # # Siguiendo el ejemplo de castilloinformatica.es (las funciones api.multi y api.one ya no funcionan / buscar alternativa)
 
-    # # Siguiendo el ejemplo de castilloinformatica.es :
     def _get_project_design(self):
         for d in self:
             d.design_id = self.env['gestion_proyectos.design'].search([('gestion_proyectos.id', '=', d.id)]).id
@@ -51,9 +56,11 @@ class Project(models.Model):
         p = self.design_id.id
         self.env['gestion_proyectos.design'].search([('id','=',p)]).write({'project':self.id})
     
-    #TODO Para acceder a los valores del design_id hay que hacer un related y una funcion/funciones con @api.onchange('campo_a_mostrar')
+    # Pagina 2. Campos del diseño. Si lo hacemos de manera predeterminada sale una emergente. Queremos mostrarlos de forma directa en la página,
+    # para ello hemos de acceder a los valores de design_id y hacer un 'related' y funciones con @api.onchange('campo_a_mostrar')
 
-    # 3. Productos y 4. Materiales - El proyecto permite seleccionar productos de tipo 'instalable':
+    # Página 3. Productos y Materiales - El proyecto permite seleccionar productos y materiales. TODO: Materiales escogidos automáticamente en función
+    # de los productos / Permitir escoger número de Productos y Materiales.
     
     # Productos con tabla intermedia para manejar el many2many en una misma tabla con materiales:
     products_ids = fields.Many2many('product.product', string= 'Productos', relation='project_product', column1='product_id', column2='project_id',
@@ -63,13 +70,13 @@ class Project(models.Model):
     materials_ids = fields.Many2many('product.product', string= 'Materiales', relation='project_materials', column1='product_id', column2='project_id',
                                      domain="[('categ_id', '=', 'Materiales')]")
 
-    # 5. Operarios:
+    # Página 4 - Operarios. Asignamos operarios al proyecto. TODO: Marcar el operario como no disponible en la fecha?
     operarios_ids = fields.Many2many('hr.employee', string= 'Operarios', required=True, domain="[('job_title', '=', 'Operario')]")
 
-    # 6. Vehiculo:
+    # Página 5 - Vehículo. Asignamos vehículos al proyecto. TODO: Marcar el vehículo como no disponible en la fecha?
     vehiculos_ids = fields.Many2many('fleet.vehicle', string = 'Vehiculo', required=True) #TODO Domain - 'Tipoinstalación'
 
-    # 7. Hitos:
+    # Página 6 - Milestones (hitos). Los proyectos tienen unos hitos asociados.
     milestones_ids = fields.Many2many('gestion_proyectos.milestone', string = "Hitos")
     
     # - Restricciones y campos computados - #
@@ -80,14 +87,15 @@ class Project(models.Model):
         for record in self:
             if record.fecha_inicio and record.fecha_inicio < fields.Date.today():
                 raise ValidationError('No se puede asignar una fecha de inicio anterior a hoy.')
-            
+    
     # Restricción para la fecha estimada de finalización:
     @api.constrains('fecha_inicio', 'fecha_estimada')
     def _check_enddate(self):
         for record in self:
             if record.fecha_estimada and record.fecha_inicio and record.fecha_estimada < record.fecha_inicio:
                 raise ValidationError("La fecha estimada de final no puede ser anterior a la fecha de inicio.")
-
+ 
+    # TODO: Realizar restricción global para que no se pueda crear un proyecto sin productos, materiales, operarios, vehiculos, diseño, etc.
     # Restricción para los productos asociados al Proyecto (mínimo un Producto)
     @api.constrains('products_ids')
     def _check_products(self):
